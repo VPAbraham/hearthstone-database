0d77e240f54f3c4989d0664fe423331ab86978ea
VPAbraham <vpa456@gmail.com>
2019-11-04 00:42:17 -0700

    Render card data to card info and link buttons to return to the main page

--- src/containers/CardInfo/CardInfo.js
+++ src/containers/CardInfo/CardInfo.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './CardInfo.scss';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export const CardInfo = (props) => {
   const { selCard } = props;
   console.log(selCard)
   return (
     <section className='card-info'>
-      <h1>-- This is card info for {selCard.name}--</h1>
+      
+      <h1>Card Data for {selCard.name}</h1>
+      <div className='info-container'>
+        <img src={selCard.imageUrl} alt='card image'/>
+        <div>
+          <p>Pokedex No. {selCard.pokedexNum}</p>
+          <p>HP: {selCard.hp}</p>
+          {/* <p>Attacks: {selCard.attacks}</p> */}
+          <p>Rarity: {selCard.rarity}</p>
+          <p>Set: {selCard.set}</p>
+          {/* <p>Type: {selCard.types[0] || 'n/a'}</p> */}
+      
+
+        </div>
+      </div>
+      <Link to='/'>
+        <button>Return to Main Page</button>
+      </Link>
     </section>
   )
 }
